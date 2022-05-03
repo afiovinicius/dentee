ad3971d7141f8e2e12b9afbd9e38d1e2527f1f31
afiovinicius <afiovinicius@gmail.com>
2022-05-03 17:54:38 -0300

    section hero desktop finalziada

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
- 
+// menu
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
