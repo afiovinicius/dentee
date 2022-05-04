e6d21e95ce7da0ca8f55c84820f1347bd70cd5c5
afiovinicius <afiovinicius@gmail.com>
2022-05-04 16:45:10 -0300

    footer finalizada com sucesso

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ btnMobile.addEventListener("touchstart", toggleMenu);
 
 // Nav Links Soft
 const softLinks = document.querySelectorAll(".js-nav li a");
-const scrollTop = document.querySelectorAll(".scroll_top");
 
 function scrollToSoft(event) {
   event.preventDefault();
@@ -20,15 +19,9 @@ function scrollToSoft(event) {
   const href = event.currentTarget.getAttribute("href");
   const section = document.querySelector(href);
   const initLink = section.offsetTop;
-  const initsTop = section.offsetTop;
 
   window.scrollTo({
-    top: initLink - 100,
-    behavior: "smooth",
-  });
-
-  window.scrollTo({
-    top: initsTop,
+    top: initLink - 20,
     behavior: "smooth",
   });
 }
@@ -37,9 +30,14 @@ softLinks.forEach((sLink) => {
   sLink.addEventListener("click", scrollToSoft);
 });
 
-scrollTop.forEach((sTop) => {
-  sTop.addEventListener("click", scrollToSoft);
-});
+// Button Scroll Top
+const scrollTop = document.getElementById("js-scroll-top"),
+  rootElement = document.documentElement;
+function scrollToTop(e) {
+  rootElement.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+scrollTop.addEventListener("click", scrollToTop);
 
 // Cards Specialties
 const hoverCard = document.querySelectorAll(".card_specialties");
