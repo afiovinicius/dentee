7387e6d38fbea9abdb52f280eae40240e72ef139
afiovinicius <afiovinicius@gmail.com>
2022-05-04 02:24:58 -0300

    secao professional finalizada

--- js/main.js
+++ js/main.js
@@ -10,6 +10,26 @@ function toggleMenu(event) {
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
 
+// Nav Links Soft
+const softLinks = document.querySelectorAll(".js-nav li a");
+
+function scrollToSoft(event) {
+  event.preventDefault();
+
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  const initLink = section.offsetTop;
+
+  window.scrollTo({
+    top: initLink - 100,
+    behavior: "smooth",
+  });
+}
+
+softLinks.forEach((sLink) => {
+  sLink.addEventListener("click", scrollToSoft);
+});
+
 // Cards Specialties
 const hoverCard = document.querySelectorAll(".card_specialties");
 
